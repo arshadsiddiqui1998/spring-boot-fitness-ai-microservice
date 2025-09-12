f5de214ee78ad54ea00931938948df90e6bb6903
arshadsiddiqui1998 <siddiquiarshad1998@gmail.com>
2025-09-13 05:18:33 +0530

    Modifying code

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -1,12 +1,8 @@
 package com.fitness.activityservice.controller;
 
 import com.fitness.activityservice.dto.ActivityDto;
-import com.fitness.activityservice.dto.ActivityRequest;
-import com.fitness.activityservice.dto.ActivityResponse;
-import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.service.ActivityService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,17 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/activities")
+@RequiredArgsConstructor
+@RequestMapping("api/v1/activities")
 public class ActivityController {
 
     private final ActivityService activityService;
 
-    public ActivityController(ActivityService activityService) {
-        this.activityService = activityService;
-    }
-
     @PostMapping
-    public ResponseEntity<ActivityDto> trackActivity(@RequestBody ActivityDto activityDto) {
+    public ResponseEntity<ActivityDto> trackActivity(@RequestBody ActivityDto activityDto){
         return ResponseEntity.ok(activityService.trackActivity(activityDto));
 
     }
--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -5,15 +5,19 @@ import com.fitness.activityservice.mapper.impl.ActivityMapperImpl;
 import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.respository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
+
     private final ActivityRepository activityRepository;
     private final ActivityMapperImpl activityMapper;
 
     public ActivityDto trackActivity(ActivityDto activityDto) {
+        log.info("In trackActivity() -> activityDto: {}", activityDto);
         Activity activity = activityMapper.dtoToEntity(activityDto);
         Activity save = activityRepository.save(activity);
         return activityMapper.entityToDto(save);
